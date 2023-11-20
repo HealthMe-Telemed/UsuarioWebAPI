aa5ec1d259fa6b57650fa9326d6509cd77564919
Vini1604 <vinicius.oliveira1998@hotmail.com>
2023-11-20 20:04:55 -0300

    Adicionado os id de medico e paciente no contrato

--- Extensions/QueryExtensions.cs
+++ Extensions/QueryExtensions.cs
@@ -18,11 +18,13 @@ namespace UsuarioWebAPI.Extensions
             WHERE U.cpf = @cpf AND U.senha = @senha;";
 
         public static string QueryBuscarPerfis() => @"
-        SELECT p.id as 'IdPerfil', p.descricao as 'Descricao' FROM perfil p
-            INNER JOIN alocacao_usuario_perfil aup
+        SELECT p.id as 'IdPerfil', p.descricao as 'Descricao', vp.paciente_id AS 'IdPaciente', vp.medico_id AS 'IdMedico' FROM healthme.perfil p
+            INNER JOIN healthme.alocacao_usuario_perfil aup
             ON aup.id_perfil = p.id
-            INNER JOIN usuario u
+            INNER JOIN healthme.usuario u
             ON aup.id_usuario = u.id
+            INNER JOIN healthme.v_perfis vp
+            ON vp.usuario_id = u.id
             WHERE u.id = @id_usuario;";
     }
     
--- Models/Perfil.cs
+++ Models/Perfil.cs
@@ -8,6 +8,8 @@ namespace UsuarioWebAPI.Models
     public class Perfil
     {
         public int IdPerfil { get; set; }
+        public int IdPaciente { get; set; }
+        public int IdMedico { get; set; }
         public string Descricao { get; set; }
     }
 }
